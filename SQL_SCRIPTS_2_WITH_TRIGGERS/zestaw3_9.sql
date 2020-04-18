42dbbebf72faab4484e8abd5adb91146cfcbc092
sebastian_bienert <226086@student.pwr.edu.pl>
2020-04-18 19:20:19 +0200

    change rule 8

--- SQL_SCRIPTS_2_WITH_TRIGGERS/zestaw3_9.sql
+++ SQL_SCRIPTS_2_WITH_TRIGGERS/zestaw3_9.sql
@@ -1,1 +1,2 @@
-Update TRACK SET UNITPRICE = 2 WHERE TRACKID = 1;
+INSERT INTO Track (TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice)
+VALUES (10000000, 'New Song', 237, 1, 1, 'Dylan, Bob', 264568, 8623572, 0.99);
